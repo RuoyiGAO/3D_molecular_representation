cd33638e2cc6fbcf61e4efba8881e7f6ed20606b
RuoyiGAO <571751325@qq.com>
2024-10-08 15:23:32 -0400

    dataset edit modify

--- data/download_small_corpus.py
+++ data/download_small_corpus.py
@@ -9,6 +9,9 @@ file_path = hf_hub_download(repo_id="thaonguyen217/FG-enhanced-SMILES_20M", file
                             repo_type="dataset")
                             #,local_dir="/scratch/eecs542f24_class_root/eecs542f24_class/gaoruoyi/data") 
 
+print("------")
+print(file_path)
+print("------")
 # Load the dataset from the .pkl file
 #RG: change the file_path
 with open(file_path, 'rb') as f:
@@ -16,3 +19,24 @@ with open(file_path, 'rb') as f:
 
 print(f'Number of FG-enhanced SMILES in the dataset: {len(dataset)}')
 print(f'Some examples: {dataset[:10]}')
+
+dataset_new = dataset[:10000]
+dataset_val = dataset[:100]
+
+#RG: get a small set of data for train
+save_path = 'FG-enhanced-SMILES_10K.pkl'
+print(f'Number of FG-enhanced SMILES in the dataset_new: {len(dataset_new)}')
+with open(save_path, 'wb') as f:
+    pickle.dump(dataset_new, f)
+txt_new = '\n'.join(dataset_new)
+with open(save_path.replace('pkl', 'txt'), 'w') as f:
+    f.write(txt_new)
+
+#RG: get a small set of data for val
+save_path_val = 'FG-enhanced-SMILES_10K_val.pkl'
+print(f'Number of FG-enhanced SMILES in the dataset_new_val: {len(dataset_val)}')
+with open(save_path_val, 'wb') as f:
+    pickle.dump(dataset_val, f)
+txt_val = '\n'.join(dataset_val)
+with open(save_path_val.replace('pkl', 'txt'), 'w') as f:
+    f.write(txt_val)
