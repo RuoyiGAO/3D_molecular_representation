82e398ed00a7330ef4d9a310ee2be92d9e832c17
RuoyiGAO <571751325@qq.com>
2024-10-07 23:47:19 -0400

    modify

--- data/download_small_corpus.py
+++ data/download_small_corpus.py
@@ -2,9 +2,15 @@ from huggingface_hub import hf_hub_download
 import pickle
 
 # Download the .pkl file from your Hugging Face dataset repo
-file_path = hf_hub_download(repo_id="thaonguyen217/FG-enhanced-SMILES_20M", filename="FG-enhanced-SMILES_20M.pkl")
+#RG: I add repo_type there, otherwise it cannot be download, there is an error
+# requests.exceptions.HTTPError: 404 Client Error 
+#RG: add an argument to change the data save path
+file_path = hf_hub_download(repo_id="thaonguyen217/FG-enhanced-SMILES_20M", filename="FG-enhanced-SMILES_20M.pkl", 
+                            repo_type="dataset")
+                            #,local_dir="/scratch/eecs542f24_class_root/eecs542f24_class/gaoruoyi/data") 
 
 # Load the dataset from the .pkl file
+#RG: change the file_path
 with open(file_path, 'rb') as f:
     dataset = pickle.load(f)
 
